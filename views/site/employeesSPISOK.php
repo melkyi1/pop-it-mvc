c5e1c5a1bdbb8ec46e741f9ae6161c54c2cbef74
root <grebenuk13@yandex.ru>
2023-03-30 15:21:08 +0300

    3 laba сделал защиту, перешёл к индивидуальным заданиям.

--- views/site/employeesSPISOK.php
+++ views/site/employeesSPISOK.php
@@ -3,7 +3,13 @@
         <a class="nazvaniePEREHODA">Список сотрудников</a> <br>
         <form method="post">
         <ol>
-            
+            <?php
+            foreach ($employees as $employee) {
+                echo '<div>';
+                echo '<p>ФИО: ' . $employee->ФИО. '</p>';
+                echo '</div>';
+            }
+            ?>
         </ol>
         </form>
     </div>
